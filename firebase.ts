80202f6a59df08a57fd6617e42fcbb0a4e104201
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-10 16:03:51 +0600

    fix deploy issue

--- firebase.ts
+++ firebase.ts
@@ -2,17 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4oZH-OEZI_HNoKYWvy1YRA8wcavPWzgA",
-  authDomain: "e-commerce-f207a.firebaseapp.com",
-  projectId: "e-commerce-f207a",
-  storageBucket: "e-commerce-f207a.appspot.com",
-  messagingSenderId: "53176714936",
-  appId: "1:53176714936:web:4fd7a63f14cab9ad08d64d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
